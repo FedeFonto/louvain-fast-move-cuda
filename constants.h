9a252e219376807ea17e0ee5cba45b4eab539a0b
Federico <federico.fontolan95@gmail.com>
2020-05-07 11:24:27 +0200

    Add modes in aggregation

--- constants.h
+++ constants.h
@@ -7,11 +7,11 @@
 #define PRINT_PERFORMANCE_LOG 1
 #define CSV_FORM 1
 
+const enum MODE { HASH, SORT, ADAPTIVE_SPEED, ADAPTIVE_MEMORY };
 const int BLOCK_SIZE = 128;
-const int WARP_SIZE = 32;
 const double MODULARITY_CONVERGED_THRESHOLD = 0.01;
 const int EARLY_STOP_LIMIT = 1000000000;
-const int N_STREAM = 1;
+
 const int STEP_ROUND = 50000000;
 const unsigned long long FLAG = 18446744073709551615;
 const unsigned int BUCKETS_SIZE = STEP_ROUND + STEP_ROUND / 3;
--- main.cu
+++ main.cu
@@ -4,10 +4,12 @@
 int main()
 {
 
-    GraphHost g = GraphHost::GraphHost("soc-LiveJournal.txt", false);
+    //GraphHost g = GraphHost::GraphHost("soc-LiveJournal.txt", false);
+    GraphHost g = GraphHost::GraphHost("graph-power-law-huge-2.edge", false);
+
 
     cudaProfilerStart();
-    auto C = ModularityAlgorithms::Laiden(g); 
+    auto C = ModularityAlgorithms::Louvain(g, SORT); 
     cudaProfilerStop();
     std::cout<< "N of community found:" << C.n_of_best_communities << std::endl;
     return 0;
