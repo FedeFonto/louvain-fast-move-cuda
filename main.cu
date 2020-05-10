2a29be3478d0003b05943019a56383c8d6d7b978
Federico <federico.fontolan95@gmail.com>
2020-05-10 12:14:54 +0200

    main multi graph
    fix

--- main.cu
+++ main.cu
@@ -3,19 +3,29 @@
 
 int main()
 {
-
-    GraphHost g = GraphHost::GraphHost("soc-LiveJournal.txt", false, 0);
+    std::vector<std::string> dataset = { "soc-LiveJournal.txt", "hollywood-2009.mtx", "europe_osm.mtx","com-orkut.ungraph.txt","uk - 2002.mtx" };  
+    std::vector<int> skip = { 0, 49, 148, 0, 48 };
+   
     //GraphHost g = GraphHost::GraphHost("graph-power-law-huge-2.edge", false, 0);
 
-    cudaProfilerStart();
-    auto C = ModularityAlgorithms::Louvain(g, HASH); 
-    std::cout << "N of community found:" << C.n_of_best_communities << std::endl <<"#####################################" << std::endl;
-    C = ModularityAlgorithms::Louvain(g, SORT);
-    std::cout << "N of community found:" << C.n_of_best_communities << std::endl <<"#####################################" << std::endl;
-    C = ModularityAlgorithms::Louvain(g, ADAPTIVE_SPEED);
-    std::cout << "N of community found:" << C.n_of_best_communities << std::endl <<"#####################################" << std::endl;
-    C = ModularityAlgorithms::Louvain(g, ADAPTIVE_MEMORY);
-    cudaProfilerStop();
-    std::cout<< "N of community found:" << C.n_of_best_communities << std::endl;
+    for(int i = 0; i< dataset.size(); i++)
+    {
+        GraphHost g = GraphHost::GraphHost("soc-LiveJournal.txt", false, 0);
+        std::cout << std::endl;
+
+        auto C = ModularityAlgorithms::Louvain(g, HASH);
+        std::cout << "N of community found:" << C.n_of_best_communities << std::endl << "#####################################" << std::endl;
+        C = ModularityAlgorithms::Louvain(g, SORT);
+        std::cout << "N of community found:" << C.n_of_best_communities << std::endl << "#####################################" << std::endl;
+        C = ModularityAlgorithms::Louvain(g, ADAPTIVE_SPEED);
+        std::cout << "N of community found:" << C.n_of_best_communities << std::endl << "#####################################" << std::endl;
+        C = ModularityAlgorithms::Louvain(g, ADAPTIVE_MEMORY);
+        std::cout << "N of community found:" << C.n_of_best_communities << std::endl;
+        std::cout << std::endl << "****************************** NEW GRAPH ******************************" << std::endl;
+
+    }
+
+
+  
     return 0;
 }
