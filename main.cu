92d6dcc7d23cf121ee2804fcf3fbdadaff757a62
Federico <federico.fontolan95@gmail.com>
2020-06-13 19:17:30 +0200

    new dataset

--- main.cu
+++ main.cu
@@ -3,8 +3,8 @@
 
 int main()
 {
-    std::vector<std::string> dataset = { "out.patentcite",  "soc-pokec-relationships.txt", "coPapersDBLP.mtx", "packing-500x100x100-b050.mtx","soc-LiveJournal.txt" ,"out.wikipedia_link_ja",  "hollywood-2009.mtx","out.wikipedia_link_it" , "out.wikipedia_link_fr","com-orkut.ungraph.txt", "out.dbpedia-link", "indochina-2004.mtx" };
-    std::vector<int> skip = { 2, 0, 129, 141, 0, 1, 49,  1, 1, 0, 2, 48 };
+    std::vector<std::string> dataset = { "out.patentcite",  "soc-pokec-relationships.txt", "coPapersDBLP.mtx", "packing-500x100x100-b050.mtx", "delaunay_n23.mtx", "soc-LiveJournal.txt" ,"out.wikipedia_link_ja",  "hollywood-2009.mtx","out.wikipedia_link_it" , "out.wikipedia_link_fr","com-orkut.ungraph.txt", "out.dbpedia-link", "indochina-2004.mtx" };
+    std::vector<int> skip = { 2, 0, 129, 141, 131, 0, 1, 49,  1, 1, 0, 2, 48 };
 
     int round = 1;
     //GraphHost g = GraphHost::GraphHost("graph-power-law-huge-2.edge", false, 0);
